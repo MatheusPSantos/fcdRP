8b710fd04ab9408160250693b1ac3614226fd074
Matheus Pereira dos Santos <pereiramatheus064@gmail.com>
2019-02-17 11:15:54 -0300

    amostragem simples e conjunto de dados iris

---
+++ amostragem_simples.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 17 11:07:14 2019
+
+@author: matheus
+"""
+
+# importando a biblioteca pandas para análise de dados e numpy para funções matemáticas
+import pandas as pd
+import numpy as np
+
+#importando o conjunto de dados em csv
